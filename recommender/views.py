0ec405f01f17b6a5573e925301e4d2cb3d0ad3d4
Vaibhav Jain <vaibhav29498@gmail.com>
2018-04-03 00:01:26 +0530

    Change basic algorithm
    Use linear regression technique (elastic net) to determine recommendations

--- recommender/views.py
+++ recommender/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from urllib.request import urlopen
 from recommender.models import Anime, Genre
 import json
+from sklearn import linear_model
+
+
+def binary_search(ls, x):
+    left = 0
+    right = len(ls) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if ls[mid] == x:
+            return mid
+        elif ls[mid] > x:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
 
 
 def index(request):
@@ -11,49 +26,58 @@ def index(request):
 def credits(request):
     return render(request, 'recommender/credits.html')
 
+
 def recommender(request):
     with urlopen('https://kuristina.herokuapp.com/anime/' + str(request.POST.get('username')) + '.json') as url:
         userdata = json.loads(url.read().decode())
     a = userdata['myanimelist']['anime']
-    anime = []
+    X = []
+    y = []
     l = []
     for x in a:
         if x['my_score'] != '0':
-            anime.append([Anime.objects.get(aid=int(x['series_animedb_id'])), int(x['my_score'])])
+            try:
+                obj = Anime.objects.get(aid=int(x['series_animedb_id']))
+            except obj.DoesNotExist:
+                continue
+            l.append(obj.aid)
+            genre = [0] * 43
+            for g in obj.genre.all():
+                genre[g.gid - 1] = 1
+            X.append([float(obj.rating), obj.members] + genre)
+            y.append(float(x['my_score']))
+        elif x['my_watched_episodes'] != '0':
             l.append(int(x['series_animedb_id']))
-    for a in anime:
-        a.append([g.gid for g in a[0].genre.all()])
 
+    if len(y) == 0:
+        err = 'No recommendations can be generated since you haven\'t rated any anime.'
+        return render(request, 'recommender/list.html', {'userid': request.POST['username'], 'error': err})
+
+    clf = linear_model.ElasticNet(alpha=0.1)
+    clf.fit(X, y)
     recommendations = []
 
-    # Parameters
-    relf = 2
-    maxrel = 1
-    userscore_weight = 1.25
-    popular_max = 2
-    popular_mid = 50000
-
     for x in Anime.objects.all():
-        y = 0
-        g = set([v.gid for v in x.genre.all()])
-        r = [v.aid for v in x.related.all()]
-        for z in anime:
-            sim = len(g.intersection(z[2])) / len(g.union(z[2]))
-            y += ((float(x.rating) * z[1]) / (userscore_weight * float(x.rating) + z[1])) * sim * (relf if z[0].aid in r else 1)
-        y *= (popular_max - (popular_mid / (popular_mid + x.members)))
-        recommendations.append([x, y, r])
+        if x.aid in l or x.members < 100:
+            continue
+        genre = [0] * 43
+        for g in x.genre.all():
+            genre[g.gid - 1] = 1
+        h = clf.predict([[x.rating, x.members] + genre])[0]
+        recommendations.append([x.aid, x.name, h])
 
-    recommendations = sorted(recommendations, key=lambda v: v[1], reverse=True)
+    recommendations = sorted(recommendations, key=lambda v: v[2], reverse=True)
     c = 0
     i = -1
     recc_id = []
     reccs = []
-    while c < 100:
+    while c < 50:
         i += 1
-        if recommendations[i][0].aid in l or len(set(recc_id).intersection(recommendations[i][2])) >= maxrel:
+        obj = Anime.objects.get(aid=recommendations[i][0])
+        if len(set(recc_id).intersection([x.aid for x in obj.related.all()])) >= 2:
             continue
-        recc_id.append(recommendations[i][0].aid)
-        reccs.append(recommendations[i][0])
+        recc_id.append(recommendations[i][0])
+        reccs.append(obj)
         c += 1
 
     return render(request, 'recommender/list.html', {'userid': request.POST['username'], 'recc': reccs})
